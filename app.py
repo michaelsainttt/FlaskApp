cfd1247a2f9ea54934076859bd24a2c2f9132c66
michaelsainttt <michaelsaint2005@gmail.com>
2026-02-12 23:46:15 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+BOOKS = [
+    {"id": 1, "title":"Don Quixote", "author": "Cervantes"},
+    {"id": 2, "title":"1984", "author": "George Orwell"},
+    {"id": 3, "title":"The Great Gatsby", "author":"F.Scott Fitzgerald"}
+]
+
+@app.route("/hello", methods=["GET"])
+def hello():
+    return jsonify({"message": "Welcome to my Flask App"}), 200
+    
+@app.route("/data", methods=["GET", "POST"])
+def data():
+    if request.method == "GET":
+        return jsonify({"books": BOOKS}), 200 
+
+    if request.method == "POST":
+        incoming = request.get_json()
+        return jsonify({"status": "success", "received": incoming}), 201
+
